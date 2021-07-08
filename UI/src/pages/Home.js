d80ea9d31848c2a2a4966ae4ebbc7bd075181b45
Klim <Severin.Klim@yandex.by>
2021-07-09 02:01:16 +0300

    Updated home page with simple card info

--- UI/src/pages/Home.js
+++ UI/src/pages/Home.js
@@ -1,5 +1,12 @@
+import FoodCard from "../components/foodCard/FoodCard";
+
 function Home() {
-    return (<h1>Home</h1>);
+    return (
+        <>
+            <h1>Home</h1>
+            <FoodCard title="Food" description="So tasty!" cookingTime="2 mins" cost="$300" />
+        </>
+    );
 }
 
 export default Home;
