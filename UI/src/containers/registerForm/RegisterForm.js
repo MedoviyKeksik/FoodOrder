1746b67854b43cde439b2d10330ed7b47095dbe9
Klim <Severin.Klim@yandex.by>
2021-07-09 01:59:40 +0300

    Updated register form state

--- UI/src/containers/registerForm/RegisterForm.js
+++ UI/src/containers/registerForm/RegisterForm.js
@@ -1,29 +1,45 @@
 import React from "react"
+import InputField from "../../components/inputField/InputField";
 
 class RegisterForm extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            name: '',
+            surname: '',
+            email: '',
+            phone: '',
+            password: ''
+        }
+
+        this.handleNameChange = this.handleChange.bind(this, 'name');
+        this.handleSurnameChange = this.handleChange.bind(this, 'surname');
+        this.handleEmailChange = this.handleChange.bind(this, 'email');
+        this.handlePhoneChange = this.handleChange.bind(this, 'phone');
+        this.handlePassswordChange = this.handleChange.bind(this, 'password');
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(field, event) {
+        const tmp = {};
+        tmp[field] = event.target.value;
+        this.setState(tmp);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        // TODO: Submit Registration data
     }
 
     render() {
         return (
-            <form>
-                <label>
-                    Name:
-                    <input type="text" name="name" />
-                </label>
-                <label>
-                    Surname:
-                    <input type="text" name="surname" />
-                </label>
-                <label>
-                    Email:
-                    <input type="email" name="email" />
-                </label>
-                <label>
-                    Phone:
-                    <input type="phone" name="phone" />
-                </label>
+            <form onSubmit={this.handleSubmit}>
+                <InputField title="Name:" type="text" name="name" isRequired={true} onChange={this.handleNameChange} />
+                <InputField title="Surname:" type="text" name="name" isRequired={true} onChange={this.handleSurnameChange} />
+                <InputField title="Email:" type="email" name="email" isRequired={true} onChange={this.handleEmailChange} />
+                <InputField title="Phone:" type="phone" name="phone" isRequired={true} onChange={this.handlePhoneChange} />
+                <InputField title="Password:" type="password" name="password" isRequired={true} onChange={this.handlePasswordChange} />
                 <input type="submit" />
             </form>
         );
