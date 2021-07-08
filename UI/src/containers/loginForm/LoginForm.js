0aa1ae03c1efdf4b12b382439ca3728d0d9030ef
Klim <Severin.Klim@yandex.by>
2021-07-08 21:50:48 +0300

    Updated Login form with state

--- UI/src/containers/loginForm/LoginForm.js
+++ UI/src/containers/loginForm/LoginForm.js
@@ -3,18 +3,40 @@ import React from "react"
 class LoginForm extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            login: '',
+            password: ''
+        }
+
+        this.handleLoginChange = this.handleLoginChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleLoginChange(event) {
+        this.setState({login: event.target.value});
+    }
+
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value});
+    }
+
+    handleSubmit(event) {
+        // TODO: Login submit
+        event.preventDefault();
     }
 
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label>
-                    Phone:
-                    <input type="phone" name="phone" />
+                    Phone/Email:
+                    <input onChange={this.handleLoginChange} type="phone" name="phone" />
                 </label>
                 <label>
                     Password:
-                    <input type="password" name="password" />
+                    <input onChange={this.handlePasswordChange} type="password" name="password" />
                 </label>
                 <input type="submit" />
             </form>
