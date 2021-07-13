281ff6185063e9023772d9cb9b5d42e7108d305c
Klim <Severin.Klim@yandex.by>
2021-07-13 03:47:56 +0300

    Added navbar localization

--- UI/src/components/navbar/Navbar.js
+++ UI/src/components/navbar/Navbar.js
@@ -1,14 +1,27 @@
+import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
 
 function Navbar(props) {
     return (
         <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/account">Account</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
-            <li><Link to="/cart">Cart</Link></li>
-            <li><Link to="/admin">Admin</Link></li>
+            <li><Link to="/">
+                <FormattedMessage defaultMessage="Home" id="app.navbar.home" />
+            </Link></li>
+            <li><Link to="/account">
+                <FormattedMessage defaultMessage="Account" id="app.navbar.account" />
+            </Link></li>
+            <li><Link to="/login">
+                <FormattedMessage defaultMessage="Login" id="app.navbar.login" />
+            </Link></li>
+            <li><Link to="/register">
+                <FormattedMessage defaultMessage="Register" id="app.navbar.register" />
+            </Link></li>
+            <li><Link to="/cart"> 
+                <FormattedMessage defaultMessage="Cart" id="app.navbar.cart" />
+            </Link></li>
+            <li><Link to="/admin">
+                <FormattedMessage defaultMessage="Admin" id="app.navbar.admin" />
+            </Link></li>
         </ul>
     );
 }
