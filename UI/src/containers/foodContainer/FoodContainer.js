09710f705c49fc06e05adb612e5f0bb5bfa46eed
Klim <Severin.Klim@yandex.by>
2021-07-20 01:37:47 +0300

    Rewrite food container to props

--- UI/src/containers/foodContainer/FoodContainer.js
+++ UI/src/containers/foodContainer/FoodContainer.js
@@ -1,42 +1,19 @@
 import React from "react";
 import FoodCard from "../../components/foodCard/FoodCard";
-import { store } from "../../store";
-import { requestFood } from "./actions";
 
-class FoodContainer extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            food: []
-        }
+function FoodContainer(props) {
+    if (props.food == null) 
+        return (<h2>No Foood!</h2>);
 
-        this.handleFoodChange = this.handleFoodChange.bind(this);
-
-        store.subscribe(this.handleFoodChange);
-    }
-
-    componentDidMount() {
-        store.dispatch(requestFood());
-    }
-
-    handleFoodChange() {
-        this.setState({
-            food: store.getState().food.items,
-            totalItemsCount: store.getState().food.totalCount
-        });
-    }
-
-    render() {
-        let cards = this.state.food.map((food) => 
-            <FoodCard key={food.id} title={food.title} description={food.description} />
-        );
-        return (
-            <div className="food-container">
-                {cards}
-            </div>
-        );
-    }
+    let cards = props.food.map((food) => 
+        <FoodCard key={food.id} title={food.title} description={food.description} />
+    );
+    return (
+        <div className="food-container">
+            {cards}
+        </div>
+    );
 }
 
 export default FoodContainer;
