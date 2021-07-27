a49884161739e85cb03e8c4918a9ecba968a5562
Klim <Severin.Klim@yandex.by>
2021-07-28 02:16:55 +0300

    Init Project

---
+++ Server/FoodOrderServer/FoodOrderServer/Controllers/FoodController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FoodOrderServer.Models;
+
+namespace FoodOrderServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private readonly FoodContext _context;
+
+        public FoodController(FoodContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Food
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Food>>> GetFood()
+        {
+            return await _context.Food.ToListAsync();
+        }
+
+        // GET: api/Food/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Food>> GetFood(int id)
+        {
+            var food = await _context.Food.FindAsync(id);
+
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            return food;
+        }
+
+        // PUT: api/Food/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFood(int id, Food food)
+        {
+            if (id != food.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(food).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FoodExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Food
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Food>> PostFood(Food food)
+        {
+            _context.Food.Add(food);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFood", new { id = food.Id }, food);
+        }
+
+        // DELETE: api/Food/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFood(int id)
+        {
+            var food = await _context.Food.FindAsync(id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            _context.Food.Remove(food);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FoodExists(int id)
+        {
+            return _context.Food.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Server/FoodOrderServer/FoodOrderServer/Models/FoodContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOrderServer.Models
+{
+    public class FoodContext : DbContext
+    {
+        public FoodContext(DbContextOptions<FoodContext> options) 
+            :base(options)
+        { 
+        }
+
+        public DbSet<Food> Food { get; set; }
+    }
+}
