538934a57e52b597a8655cbbc6a770a7191334f8
Klim <Severin.Klim@yandex.by>
2021-08-02 21:40:29 +0300

    Added context interface

---
+++ Server/FoodOrderServer/FoodOrderServer.DataAccess/Contexts/IGenericContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrderServer.DataAccess.Contexts
+{
+    interface IGenericContext<T> where T : class
+    {
+        DbSet<T> Data { get; set; }
+    }
+}
