d4d65edefbd74b3275b32b7286418aec6929448e
Klim <Severin.Klim@yandex.by>
2021-08-02 21:40:03 +0300

    Added repository pattern class and interface

---
+++ Server/FoodOrderServer/FoodOrderServer.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using FoodOrderServer.DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrderServer.DataAccess.Repositories
+{
+    class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(IGenericContext<T> dbContext) {
+            _dbSet = dbContext.Data;
+        }
+
+        public async Task Add(T item)
+        {
+            await _dbSet.AddAsync(item);
+        }
+
+        public void Delete(int id)
+        {
+            T item = _dbSet.Find(id);
+            if (item != null) {
+                _dbSet.Remove(item);
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Get(int id)
+        {
+            await _dbSet.FindAsync(id);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public void Update(T item)
+        {
+            _dbSet.Update(item);
+        }
+    }
+}
---
+++ Server/FoodOrderServer/FoodOrderServer.DataAccess/Repositories/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrderServer.DataAccess
+{
+    interface IGenericRepository<T> : IDisposable where T : class
+    {
+        Task Add(T item);
+
+        Task Get(int id);
+        IQueryable<T> GetAll();
+        void Update(T item);
+        void Delete(int id);
+    }
+}
